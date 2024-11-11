f64f2e3b7f19fe7915f056f3a386af9d03b947ba
korezawa1005 <t.k.oshihiki@gmail.com>
2024-11-11 22:15:16 +0900

    Add password reset

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.time_zone = 'Asia/Tokyo'
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,5 +19,6 @@ module SampleApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.default_timezone = :local
   end
 end
